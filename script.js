0da57d0fa8d8d8f0cfb2ab4ad7607a004eae2808
Vanessa-Peixoto <vanessa.peixoto.dev@gmail.com>
2021-11-21 00:46:01 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+$(function(){
+	$('h1').mouseenter(function(){
+		$('p').toggle('slow');
+	});
+});
+
+$(function(){
+$('button').click(function(){
+	$('.button').animate({
+		left:'600px',
+		opacity: 0.2,
+	}, 5000)
+}
+
+	)
+	})
